c58b068c059656b6c7640f34fbdd7c1e78509f74
Antonio Barcelos <antonio.barcelos@neo4j.com>
2021-07-29 15:45:01 +0200

    Update testkit build to use the lerna

--- testkit/integration.py
+++ testkit/integration.py
@@ -2,25 +2,18 @@ import subprocess
 import os
 
 
-def run(args, cwd):
+def run(args):
     subprocess.run(
         args, universal_newlines=True, 
-        stderr=subprocess.STDOUT, check=True, cwd=cwd)
-
-
-def test_driver():
-    run(["gulp", "test-browser"], "./packages/neo4j-driver")
-    run(["gulp", "test-nodejs-integration"], "./packages/neo4j-driver")
-
-
-def test_driver_lite():
-    run(["npm", "run", "test:it"], "./packages/neo4j-driver-lite")
-    run(["npm", "run", "test:it:browser"], "./packages/neo4j-driver-lite")
+        stderr=subprocess.STDOUT, check=True)
 
 
 if __name__ == "__main__":
     os.environ["TEST_NEO4J_IPV6_ENABLED"] = "False"
+
     if os.environ.get("TEST_DRIVER_LITE", False):
-        test_driver_lite()
+        ignore = "--ignore=neo4j-driver"
     else:
-        test_driver()
+        ignore = "--ignore=neo4j-driver-lite"
+
+    run(["npm", "run", "test::integration", "--", ignore])
