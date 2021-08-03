a53db7824b82b0d7264fa2c701fb64d934ab076d
Antonio Barcelos <antonio.barcelos@neo4j.com>
2021-08-03 16:04:00 +0200

    Copy driver to insider testkit container

--- testkit/integration.py
+++ testkit/integration.py
@@ -1,19 +1,14 @@
-import subprocess
+
 import os
-
-
-def run(args):
-    subprocess.run(
-        args, universal_newlines=True, 
-        stderr=subprocess.STDOUT, check=True)
+from common import run_in_driver_repo, is_lite
 
 
 if __name__ == "__main__":
     os.environ["TEST_NEO4J_IPV6_ENABLED"] = "False"
 
-    if os.environ.get("TEST_DRIVER_LITE", False):
+    if is_lite():
         ignore = "--ignore=neo4j-driver"
     else:
         ignore = "--ignore=neo4j-driver-lite"
 
-    run(["npm", "run", "test::integration", "--", ignore])
+    run_in_driver_repo(["npm", "run", "test::integration", "--", ignore])
